f59aa8a4e241b15d1dcfd48d10a10960a8520379
usr <rlarlgus0206@naver.com>
2023-11-03 21:44:40 +0900

    update : 디스패처 서블릿 적용

--- src/main/java/hellospring/tobyspring/TobyspringApplication.java
+++ src/main/java/hellospring/tobyspring/TobyspringApplication.java
@@ -3,22 +3,14 @@ package hellospring.tobyspring;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class TobyspringApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();   //dispactherServlet 때문에 변경함
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
@@ -26,29 +18,11 @@ public class TobyspringApplication {
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    //인증, 보안, 다국어, 공통 기능을 모두 구현했다고 가정함
-                    //기존에는 URL을 매칭해서 Servlet Container가 Servlet에게 요청을 위임했다.
-                    //이제는 그 역할을 이 프론트 컨트롤러에서 해줄 수 있어야 한다
-
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else if (req.getRequestURI().equals("/uesr")) {
-
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");    //모든 url을 여기서 처리할 것이다
+            servletContext.addServlet("dispatcherServlet",
+                    /*DispatcherServlet 이 매칭을 하다가 작업을 위임함
+                    요청을 디스패치할 오브젝트를 찾아야되는데 그때 사용할 ServletContainer 를 전달해줌*/
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         });
         webServer.start();
     }
