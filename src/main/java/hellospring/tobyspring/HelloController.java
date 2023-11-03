6e30cacfca141590d90aa645266094518ffb1253
usr <rlarlgus0206@naver.com>
2023-11-03 22:22:56 +0900

    update : fin

--- src/main/java/hellospring/tobyspring/HelloController.java
+++ src/main/java/hellospring/tobyspring/HelloController.java
@@ -1,7 +1,12 @@
 package hellospring.tobyspring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +14,9 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody   //String이 리턴되면 view로 받아들이기 때문에 ~~.html 파일을 반환하지 않아도 되도록 해주는 애노테이션
+    //@RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
