03a8b01abeb2f6a3266e2b8e24540bcd180f5f25
usr <rlarlgus0206@naver.com>
2023-11-03 21:14:13 +0900

    update : DI 적용

--- src/main/java/hellospring/tobyspring/HelloController.java
+++ src/main/java/hellospring/tobyspring/HelloController.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class HelloController {
     private final HelloService helloService;
 
+    // 생성자를 통해 Bean을 만들 때 어떤 것이 필요한지 알려준다
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
