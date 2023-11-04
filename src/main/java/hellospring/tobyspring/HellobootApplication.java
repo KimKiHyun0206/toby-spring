fb681ea9ed22f23599e04082d11a3eac04616cb9
usr <rlarlgus0206@naver.com>
2023-11-04 09:51:20 +0900

    update : 생성자를 통한 주입

--- src/main/java/hellospring/tobyspring/HellobootApplication.java
+++ src/main/java/hellospring/tobyspring/HellobootApplication.java
@@ -3,6 +3,7 @@ package hellospring.tobyspring;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,18 +13,30 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class HellobootApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                //dispatcherServlet.setApplicationContext(this);    //없어도 잘 동작한다
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet
                     /*DispatcherServlet 이 매칭을 하다가 작업을 위임함
                     요청을 디스패치할 오브젝트를 찾아야되는데 그때 사용할 ServletContainer 를 전달해줌*/
-                            new DispatcherServlet(this)
                     ).addMapping("/*");
                 });
                 webServer.start();
