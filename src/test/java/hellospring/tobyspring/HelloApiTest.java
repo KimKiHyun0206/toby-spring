eabc6ce25fe6b715c7dd71b1b69eb30064756685
usr <rlarlgus0206@naver.com>
2023-11-04 14:33:03 +0900

    update : 테스트 코드 작성

---
+++ src/test/java/hellospring/tobyspring/HelloApiTest.java
@@ -0,0 +1,33 @@
+package hellospring.tobyspring;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        //http localhost:8080/hello?name=Spring
+        //Httpie 라고 부르기를 개발자들이 요청한다(터미널)
+
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> response =
+                rest.getForEntity(
+                        "http://localhost:8080/hello?name={name}",
+                        String.class,
+                        "Spring"
+                );
+        //status code 200
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //header(content-type) test/plain
+        Assertions.assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        //body Hello Spring
+        Assertions.assertThat(response.getBody()).isEqualTo("Hello Spring");
+    }
+}
