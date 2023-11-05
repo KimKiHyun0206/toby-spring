32bb021512fc0e66d8df79dc19049f03d3bb1e5e
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-11-05 21:39:24 +0900

    test1

--- src/main/java/hellospring/tobyspring/HellobootApplication.java
+++ src/main/java/hellospring/tobyspring/HellobootApplication.java
@@ -1,14 +1,8 @@
 package hellospring.tobyspring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
+@SpringBootAnnotation
 public class HellobootApplication {
 
     public static void main(String[] args) {
