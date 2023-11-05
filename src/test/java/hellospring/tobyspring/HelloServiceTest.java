165d841139f7c9277bd3fe798b31c2a862e8b551
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-11-05 20:48:25 +0900

    update : Meta Annotation

--- src/test/java/hellospring/tobyspring/HelloServiceTest.java
+++ src/test/java/hellospring/tobyspring/HelloServiceTest.java
@@ -3,6 +3,26 @@ package hellospring.tobyspring;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest{    //이렇게 하면 Test를 따로 할 수 있는 메타 애노테이션이 만들어진다
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest{    //메타애노테이션은 재귀가 가능하다
+
+}
+
+
 public class HelloServiceTest {
 
     @Test
