7f4504d423e157f3f846d301ed057ce981ae2f1b
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-11-08 20:42:16 +0900

    update : MyAutoConfiguration 자동 구성 변경

--- src/main/java/hellospring/tobyspring/config/MyAutoConfiguration.java
+++ src/main/java/hellospring/tobyspring/config/MyAutoConfiguration.java
@@ -9,7 +9,12 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false)    //기본값 = true
+/*
+@MyAutoConfiguration 애노테이션을 붙여서 imports 파일을 통해서 동적으로 로딩되는 Configuration 들은 기존에 우리가 사용했던 것처럼
+그냥 Configuration Annotation 이 적용되는 것이 아니라 ProxyBeanMethods 를 false  바꾼 Configuration 이 적용된다
+*/
+//리펙토링을 할 때는 시스템이 변경되지는 않았는지 점검하는 것이 꼭 필요하다
 public @interface MyAutoConfiguration {
 
 }
