8d2c94357c2cea12cf9acc47067c643b895aff59
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-11-17 18:27:04 +0900

    update : Jetty Configuration and Bean name

---
+++ src/main/java/hellospring/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -0,0 +1,15 @@
+package hellospring.tobyspring.config.autoconfig;
+
+import hellospring.tobyspring.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@MyAutoConfiguration
+public class JettyWebServerConfig {
+
+    @Bean(name = "jettyWebServerFactory")
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new JettyServletWebServerFactory();
+    }
+}
