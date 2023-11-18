08e0c9afdeedd61e8456d5a88ecdc4d0be56a3f9
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-11-18 15:37:21 +0900

    update : Conditional

--- src/main/java/hellospring/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/hellospring/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -4,12 +4,24 @@ import hellospring.tobyspring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
 
     @Bean(name = "jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;   //true면 이 클래스가 빈으로 등록되는 것이다
+        }
     }
 }
