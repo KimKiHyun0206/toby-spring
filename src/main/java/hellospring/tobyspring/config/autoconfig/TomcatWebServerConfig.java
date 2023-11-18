5631e29aff36efd0d5a382c3ecb2cbac722ad1ac
usr <rlarlgus0206@naver.com>
2023-11-18 22:04:51 +0900

    update : @ConditionalOnMissingBean

--- src/main/java/hellospring/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/hellospring/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package hellospring.tobyspring.config.autoconfig;
 
 import hellospring.tobyspring.config.ConditionalMyOnClass;
 import hellospring.tobyspring.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.util.ClassUtils;
 public class TomcatWebServerConfig {
 
     @Bean(name = "tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
